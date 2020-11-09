76bf9acc23337e2ee1bd01d16752d81f58a1288f
Angela Guardian <jillguardian@gmail.com>
2020-11-09 17:23:53 +0800

    Add event handler to search input

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,11 +2,25 @@ import React from 'react'
 
 export default class Search extends React.Component {
 
+    state = {
+        term: ''
+    }
+
+    onSearch = (event) => {
+        event.preventDefault();
+        // TODO: Pass value to callback
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.onSearch}>
                 <div className="ui icon input">
-                    <input className="prompt" type="text" placeholder="Search video..." />
+                    <input
+                        className="prompt"
+                        type="text"
+                        placeholder="Search video..."
+                        onChange={event => this.setState({term: event.target.value})}
+                        value={this.state.term} />
                     <i className="search icon" />
                 </div>
             </form>

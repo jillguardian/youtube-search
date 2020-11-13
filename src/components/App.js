f040377c35bd864ea0428cd5704431d5f50346ac
Angela Guardian <jillguardian@gmail.com>
2020-11-13 17:03:19 +0800

    Call search endpoint on form submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import Search from "./Search";
+import youtube from "../api/youtube";
 
 export default class App extends React.Component {
 
-    onSearch(term) {
-        // TODO: Call API
+    state = {
+        videos: []
+    }
+
+    onSearch = async term => {
+        const response = await youtube.get('/search', {
+            params: {
+                part: 'snippet',
+                maxResults: 20,
+                type: 'video',
+                q: term
+            }
+        })
+        const data = response.data;
+        this.setState({
+            videos: data.items
+        });
     }
 
     render() {
@@ -13,7 +29,7 @@ export default class App extends React.Component {
                 <div className="ui equal width center aligned padded grid">
                     <div className="row">
                         <div className="column">
-                            <Search onSearch={this.onSearch} />
+                            <Search onSearch={this.onSearch}/>
                         </div>
                     </div>
                 </div>
